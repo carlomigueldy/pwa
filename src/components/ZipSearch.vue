e9736214ed83984c6ed9991e2fc9554823fc4d4c
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-08-18 06:58:15 +0800

    installed Ionic

---
+++ src/components/ZipSearch.vue
@@ -0,0 +1,35 @@
+<template>
+  <ion-grid>
+      <form @submit="onSubmit">
+        <ion-col>
+            <ion-item>
+                <ion-label>Zipcode: </ion-label>
+                <ion-input :value="zip" @input="zip = $event.target.value"
+                 name="zip" placeholder="Enter US Zipcode" />
+            </ion-item>
+        </ion-col>
+        <ion-col>
+          <ion-button type="submit" 
+          color="primary" 
+          expand="block">Find</ion-button>
+        </ion-col>
+      </form>
+  </ion-grid>
+</template>
+
+<script>
+export default {
+  name: 'ZipSearch',
+  data() {
+    return {
+      zip: '',
+    }
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault()
+      const isValidZip = /(^\d{5}$)|(^\d{5}--\d{4}$)/.test(this.zip)
+    },
+  },
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>PWA</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <ZipSearch />
+    </ion-content>
+  </div>
+</template>
+
+<script>
+import ZipSearch from '../components/ZipSearch'
+
+export default {
+  name: 'home',
+  components: { ZipSearch }
+}
+</script>
