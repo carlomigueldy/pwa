1a487847842dca65c1cef7f0d6c9ecafa63a9ac9
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-08-18 09:30:25 +0800

    Added clear info

---
+++ src/components/ClearInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <ion-button color="light" expand="block" 
+  @click="$emit('clear-info')" v-if="info">
+  Clear
+  </ion-button>
+</template>
+
+<script>
+export default {
+    name: 'ClearInfo',
+    props: ['info'],
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,20 +7,46 @@
     </ion-header>
     <ion-content class="ion-padding">
       <ZipSearch v-on:get-zip="getZipInfo" />
+      <ClearInfo :info="info" v-on:clear-info="clearInfo" />
+      <ZipInfo :info="info" />
     </ion-content>
   </div>
 </template>
 
 <script>
 import ZipSearch from '../components/ZipSearch'
+import ZipInfo from '../components/ZipInfo'
+import ClearInfo from '../components/ClearInfo'
 
 export default {
   name: 'home',
-  components: { ZipSearch },
+  components: { ZipSearch, ZipInfo, ClearInfo },
+  data() {
+    return {
+      info: null,
+    }
+  },
   methods: {
     async getZipInfo(zip) {
       const res = await fetch(`https://api.zippopotam.us/us/${zip}`) 
-    }
+      if (res.status == 404) {
+        this.showAlert()
+        return
+      }
+      this.info = await res.json()
+    },
+    showAlert() {
+      return this.$ionic.alertController
+        .create({
+          header: "Enter Zipcode",
+          message: "Please enter a valid US Zipcode",
+          buttons: ["OK"] 
+        })
+        .then(a => a.present())
+    },
+    clearInfo() {
+      this.info = null
+    },
   },
 }
 </script>
